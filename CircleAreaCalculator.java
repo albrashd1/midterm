0a2e5f7048e417540ee803e44f15595bf5fe45aa
Haitham <sinaw@umich.edu>
2023-03-09 14:36:59 -0500

    YourBranch second class

--- CircleAreaCalculator.java
+++ CircleAreaCalculator.java
@@ -5,3 +5,11 @@ public class CircleAreaCalculator {
         System.out.println("The area of the circle is " + area);
     }
 }
+
+public class CircleAreaCalculator2 {
+    public static void main(String[] args) {
+        double radius = 5.0; // radius of the circle
+        double area = Math.PI * radius * radius; // formula for area of a circle
+        System.out.println("The area of the circle is " + area);
+    }
+}
